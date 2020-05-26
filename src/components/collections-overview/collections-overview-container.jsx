ddfdf37ebd338c2ea98383228913a127fed97810
Anthony Curtis <tonydc1997@gmail.com>
2020-05-26 12:41:42 -0400

    feat: Create Get_Collections query

--- src/components/collections-overview/collections-overview-container.jsx
+++ src/components/collections-overview/collections-overview-container.jsx
@@ -4,3 +4,18 @@ import { gql } from "apollo-boost";
 
 import CollectionsOverview from "./collections-overview.component";
 import Spinner from "../spinner/spinner.component";
+
+const GET_COLLECTIONS = gql`
+  {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
