cf1300315d0c0e75c43aebe0a97343db0f77a7b9
Anthony Curtis <tonydc1997@gmail.com>
2020-05-26 12:25:06 -0400

    chore: Make necessary imports

---
+++ src/components/collections-overview/collections-overview-container.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import CollectionsOverview from "./collections-overview.component";
+import Spinner from "../spinner/spinner.component";
