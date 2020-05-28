02ed0ca98ed028855886a1282e0d21c9af5cc963
Anthony Curtis <tonydc1997@gmail.com>
2020-05-28 13:10:10 -0400

    feat: Make CollectionsOverview into explicit return
    - Move query into CollectionsOverview.
    
    - Remove redundant import.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,32 +3,33 @@ import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
 import CollectionPreview from "../collection-preview/collection-preview.component";
-import CollectionsOverview from "./collections-overview.component";
 import Spinner from "../spinner/spinner.component";
 
 import "./collections-overview.styles.scss";
 
-const GET_COLLECTIONS = gql`
-  {
-    collections {
-      id
-      title
-      items {
+const CollectionsOverview = ({ collections }) => {
+  const GET_COLLECTIONS = gql`
+    {
+      collections {
         id
-        name
-        price
-        imageUrl
+        title
+        items {
+          id
+          name
+          price
+          imageUrl
+        }
       }
     }
-  }
-`;
+  `;
 
-const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
-);
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
 export default CollectionsOverview;
