d3177e8c74d1f0eb5fd417244e5a471c58ecc694
Anthony Curtis <tonydc1997@gmail.com>
2020-05-28 13:07:48 -0400

    feat: Copy/paste code from container component
    - Replace `Query` import w/ `useQuery from @apollo/react-hooks`

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
 import CollectionPreview from "../collection-preview/collection-preview.component";
+import CollectionsOverview from "./collections-overview.component";
+import Spinner from "../spinner/spinner.component";
 
 import "./collections-overview.styles.scss";
+
+const GET_COLLECTIONS = gql`
+  {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
