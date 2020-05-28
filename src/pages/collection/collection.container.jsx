1064ce1814342f7737e62ef87b25cf4edccfdde3
Anthony Curtis <tonydc1997@gmail.com>
2020-05-28 12:22:20 -0400

    feat: Build out graphQL query
    - Get collection by title (dynamically).

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -4,3 +4,18 @@ import { gql } from "apollo-boost";
 
 import CollectionPage from "./collection.component";
 import Spinner from "../../components/spinner/spinner.component";
+
+const GET_COLLECTION_BY_TITLE = gql`
+  query getCollectionByTitle($title: String!) {
+    getCollectionByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
