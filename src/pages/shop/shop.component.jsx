92c29672306d1da5e89b6022a65942f6be04ab52
Anthony Curtis <tonydc1997@gmail.com>
2020-06-02 11:47:44 -0400

    feat: Revert CollectionPage import to prev iteration

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import { default as CollectionPage } from "../collection/collection.container";
+import CollectionPage from "../collection/collection.component";
 import "./shop.styles.scss";
 
 const ShopPage = ({ match }) => (
