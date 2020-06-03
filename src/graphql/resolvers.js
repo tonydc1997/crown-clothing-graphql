1d74cdb7a6c5a773c00fc5184efc633f30173c51
Anthony Curtis <tonydc1997@gmail.com>
2020-06-03 09:15:25 -0400

    feat: Define logic for updating state/writing to query

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -18,6 +18,11 @@ export const resolvers = {
       const { cartHidden } = cache.readQuery({
         query: GET_CART_HIDDEN,
       });
+
+      cache.writeQuery({
+        query: GET_CART_HIDDEN,
+        data: { cartHidden: !cartHidden },
+      });
     },
   },
 };
