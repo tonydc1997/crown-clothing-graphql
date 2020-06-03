f0603195145571542ae07611c785714baceb39fe
Anthony Curtis <tonydc1997@gmail.com>
2020-06-03 12:47:57 -0400

    feat: Remove redux bindings and imports

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
-import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({ item }) => {
   const { name, price, imageUrl } = item;
 
   return (
@@ -28,8 +26,8 @@ const CollectionItem = ({ item, addItem }) => {
   );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  addItem: (item) => dispatch(addItem(item)),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   addItem: (item) => dispatch(addItem(item)),
+// });
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default CollectionItem;
