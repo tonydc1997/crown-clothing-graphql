56bf3e719805df49167afd55e3357614ecc6fd1a
Anthony Curtis <tonydc1997@gmail.com>
2020-06-05 13:16:27 -0400

    feat: Implement AddItem w/ Apollo
    - Import gql and Apollo Hook.
    
    - Define Mutation query, then grab it.
    
    - Rename method within CheckoutItem.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
+import { gql } from "apollo-boost";
+import { useMutation } from "react-apollo";
 
 import {
   clearItemFromCart,
@@ -10,6 +12,13 @@ import {
 import "./checkout-item.styles.scss";
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const ADD_ITEM_TO_CART = gql`
+    mutation AddItemToCart($item: Item!) {
+      addItemToCart(item: $item) @client
+    }
+  `;
+  const [addItemToCart] = useMutation(ADD_ITEM_TO_CART);
+
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
@@ -22,7 +31,7 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
           &#10094;
         </div>
         <span className="value">{quantity}</span>
-        <div className="arrow" onClick={() => addItem(cartItem)}>
+        <div className="arrow" onClick={() => addItemToCart(cartItem)}>
           &#10095;
         </div>
       </span>
