33c840541f9f5d30b122e823721d0987b27ee84c
Anthony Curtis <tonydc1997@gmail.com>
2020-06-05 12:47:52 -0400

    feat: Gain ability to destructure states from queries
    - Ensure Checkout uses explicit return.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 import CreditCard from "../../assets/pay.svg";
 import CardExp from "../../assets/icon-calendar-date.svg";
@@ -9,34 +11,36 @@ import StripeCheckoutButton from "../../components/stripe-button/stripe-button.c
 
 import "./checkout.styles.scss";
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <div className="total">
-      Your total is
-      <span className="totalPrice"> ${total}</span>
+const CheckoutPage = ({ cartItems, total }) => {
+  return (
+    <div className="checkout-page">
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">
+        Your total is
+        <span className="totalPrice"> ${total}</span>
+      </div>
+      <div className="test-warning">
+        <p className="test-title">
+          Please use the credit card below when making payments.
+        </p>
+        <span className="test-message">
+          <img src={CreditCard} alt="Credit Card icon" />
+          4242 4242 4242 4242
+          <br />
+          <img src={CardExp} alt="Calender Icon" />
+          01/30
+          <br />
+          <img src={CardCVC} alt="Secure Lock" />
+          123
+        </span>
+      </div>
+      <div className="button-container">
+        <StripeCheckoutButton price={total} />
+      </div>
     </div>
-    <div className="test-warning">
-      <p className="test-title">
-        Please use the credit card below when making payments.
-      </p>
-      <span className="test-message">
-        <img src={CreditCard} alt="Credit Card icon" />
-        4242 4242 4242 4242
-        <br />
-        <img src={CardExp} alt="Calender Icon" />
-        01/30
-        <br />
-        <img src={CardCVC} alt="Secure Lock" />
-        123
-      </span>
-    </div>
-    <div className="button-container">
-      <StripeCheckoutButton price={total} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default CheckoutPage;
