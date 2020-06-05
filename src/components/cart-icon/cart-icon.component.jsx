a4d50e0eb586773d03f62db3e1abe5a7639211c9
Anthony Curtis <tonydc1997@gmail.com>
2020-06-05 09:45:25 -0400

    feat: Import `useQuery` hook

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { gql } from "apollo-boost";
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
