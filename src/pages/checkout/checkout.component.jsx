084c3e03817231ce9b5870b7622879a30c2db3d9
Anthony Curtis <tonydc1997@gmail.com>
2020-06-05 12:45:00 -0400

    feat: Remove redux bindings & imports
    - Begin GraphQL/Apollo implementation.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
 import CreditCard from "../../assets/pay.svg";
 import CardExp from "../../assets/icon-calendar-date.svg";
@@ -8,11 +6,6 @@ import CardCVC from "../../assets/icon-lock.svg";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
-
-import {
-  selectCartItems,
-  selectCartTotal,
-} from "../../redux/cart/cart.selectors";
 
 import "./checkout.styles.scss";
 
@@ -46,9 +39,4 @@ const CheckoutPage = ({ cartItems, total }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal,
-});
-
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
