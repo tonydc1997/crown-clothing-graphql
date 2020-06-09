66a4b0df3bd21eff714eb62f67ae32b4d3c9b2f0
Anthony Curtis <tonydc1997@gmail.com>
2020-06-09 11:58:05 -0400

    feat: Wrap App in ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -49,20 +49,22 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
+          <ErrorBoundary>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
+            <Route
+              exact
+              path="/signin"
+              render={() =>
+                this.props.currentUser ? (
+                  <Redirect to="/" />
+                ) : (
+                  <SignInAndSignUpPage />
+                )
+              }
+            />
+          </ErrorBoundary>
         </Switch>
       </div>
     );
