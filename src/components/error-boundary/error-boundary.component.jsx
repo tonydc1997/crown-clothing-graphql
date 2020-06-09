ab0c59b43bcf0be08ccd301ef269e460d8dfcf04
Anthony Curtis <tonydc1997@gmail.com>
2020-06-09 11:48:35 -0400

    feat: Create error-boundary
    - Create folder/file structure.
    
    - Use errorBoundary from similar project.

---
+++ src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import astronaut from "../../assets/images/astronaut.png";
+
+class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasErrored: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // process the error & return fallback UI
+    return { hasErrored: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <div className="error-image-overlay">
+          <img src={astronaut} alt="An astronaut lost in space" />
+          <p className="error-text">Sorry, this page is lost in space...</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
