c3dae348ae05f153c52e5bf1caf25625fdb11705
Anthony Curtis <tonydc1997@gmail.com>
2020-06-11 09:00:09 -0400

    feat: Handle server, network, and GraphQL errors

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -59,6 +59,17 @@ const updateCartItemsRelatedQueries = (cache, newCartItems) => {
   });
 };
 
+const handleError = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 export const resolvers = {
   Mutation: {
     toggleCartHidden: (_root, _args, { cache }) => {
